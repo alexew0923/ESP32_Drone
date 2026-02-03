4c3153946f0919b818706246689ae884c9c98477
alexew0923 <70456996+alexew0923@users.noreply.github.com>
2026-02-03 16:09:55 -0400

    Update Drone_Receiver.ino

--- Drone_Receiver/Drone_Receiver.ino
+++ Drone_Receiver/Drone_Receiver.ino
@@ -1,42 +1,65 @@
 #include <esp_now.h>
 #include <WiFi.h>
 
-#define M1_PIN 19
-#define M2_PIN 21
-#define M3_PIN 22
-#define M4_PIN 23
+#define M1_PIN 1
+#define M2_PIN 4
+#define M3_PIN 7
+#define M4_PIN 8
 
 // Structure example to receive data
 // Must match the sender structure
 typedef struct struct_message {
-  int M1;
-  int M2;
-  int M3;
-  int M4;
+  float roll;
+  float pitch;
+  float yaw;
+  int thrust;
 } struct_message;
 
-// Create a struct_message called myData
-struct_message myData;
+// Create a struct_message called droneData
+struct_message droneData;
 
 // callback function that will be executed when data is received
 void OnDataRecv(const uint8_t * mac, const uint8_t *incomingData, int len) {
-  memcpy(&myData, incomingData, sizeof(myData));
+  memcpy(&droneData, incomingData, sizeof(droneData));
   Serial.print("Bytes received: ");
   Serial.println(len);
-  Serial.print("M1: ");
-  Serial.println(myData.M1);
-  Serial.print("M2: ");
-  Serial.println(myData.M2);
-  Serial.print("M3: ");
-  Serial.println(myData.M3);
-  Serial.print("M4: ");
-  Serial.println(myData.M4);
+  Serial.print("Roll: ");
+  Serial.println(droneData.roll);
+  Serial.print("Pitch: ");
+  Serial.println(droneData.pitch);
+  Serial.print("Yaw: ");
+  Serial.println(droneData.yaw);
+  Serial.print("Thrust: ");
+  Serial.println(droneData.thrust);
   Serial.println();
 
-  analogWrite(M1_PIN, myData.M4);
-  analogWrite(M2_PIN, myData.M4);
-  analogWrite(M3_PIN, myData.M4);
-  analogWrite(M4_PIN, myData.M4);
+  int M1_strength = droneData.thrust; //+ droneData.thrust*-1*droneData.yaw + droneData.thrust*-1*droneData.roll + droneData.thrust*-1*droneData.pitch;
+  int M2_strength = droneData.thrust; //+ droneData.thrust*droneData.yaw + droneData.thrust*droneData.roll + droneData.thrust*-1*droneData.pitch;
+  int M3_strength = droneData.thrust; //+ droneData.thrust*droneData.yaw + droneData.thrust*-1*droneData.roll + droneData.thrust*droneData.pitch;
+  int M4_strength = droneData.thrust; //+ droneData.thrust*-1*droneData.yaw + droneData.thrust*droneData.roll + droneData.thrust*droneData.pitch;
+  M1_strength = round(M1_strength);
+  M1_strength = constrain(M1_strength, 0, 255);
+  M2_strength = round(M2_strength);
+  M2_strength = constrain(M2_strength, 0, 255);
+  M3_strength = round(M3_strength);
+  M3_strength = constrain(M3_strength, 0, 255);
+  M4_strength = round(M4_strength);
+  M4_strength = constrain(M4_strength, 0, 255);
+
+  Serial.print("M1: ");
+  Serial.println(M1_strength);
+  Serial.print("M2: ");
+  Serial.println(M2_strength);
+  Serial.print("M3: ");
+  Serial.println(M3_strength);
+  Serial.print("M4: ");
+  Serial.println(M4_strength);
+  Serial.println();
+  
+  analogWrite(M1_PIN, M1_strength);
+  analogWrite(M2_PIN, M2_strength);
+  analogWrite(M3_PIN, M3_strength);
+  analogWrite(M4_PIN, M4_strength);
 }
  
 void setup() {
